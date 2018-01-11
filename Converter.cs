2cc7d8e2dfaeb46dc258535f362b915ac7610e40
ABanks28 <riverbanks456@gmail.com>
2018-01-11 12:54:09 -0500

    REAL CHOP BRUH BRUH
    lol

---
+++ Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Convert
+{
+     class Converter
+     {
+         static void Main()
+         {
+            Console.WriteLine("Infix expression: ");
+            String infix = Console.ReadLine();
+            infix = infix.Replace(" ", "");
+
+            for (int i = 0; i < infix.Length; i++)
+            {
+                String t = infix.Substring(i, i + 1);
+                if (t == "+" || t == "-" || t == "*" || t == "/")
+                {
+                    return t;
+                }
+            }
+            Stack st = new Stack();
+             
+         }
+     }
+}
