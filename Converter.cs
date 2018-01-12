2c21477245971e08db5358dfde38a569540d1fdb
ABanks28 <riverbanks456@gmail.com>
2018-01-12 11:42:21 -0500

    reefdgfdfdsafds
    fdsafdsa

--- Converter.cs
+++ Converter.cs
@@ -7,24 +7,33 @@ using System.Collections;
 
 namespace Convert
 {
-     class Converter
-     {
-         static void Main()
-         {
+    class Converter
+    {
+        static void Main()
+        {
             Console.WriteLine("Infix expression: ");
             String infix = Console.ReadLine();
             infix = infix.Replace(" ", "");
+            String postfix = "";
+            Stack st = new Stack();
 
             for (int i = 0; i < infix.Length; i++)
             {
                 String t = infix.Substring(i, i + 1);
+                
+                if (t != "+" || t != "-" || t != "*" || t != "/")
+                {
+                    postfix = postfix + t;
+                }
                 if (t == "+" || t == "-" || t == "*" || t == "/")
                 {
-                    return t;
+                    st.Push(t);
                 }
+
             }
-            Stack st = new Stack();
-             
-         }
-     }
+
+            Console.WriteLine(postfix);
+            Console.ReadLine();
+        }
+    }
 }
